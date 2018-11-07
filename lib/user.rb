4901a9b5387e80126992b0cc9718bf46aeecb70d
Tolvic <terence.h.mace@gmail.com>
2018-11-07 16:03:00 +0000

    user class methods written with tests. 5 tests not passing

--- lib/user.rb
+++ lib/user.rb
@@ -31,9 +31,35 @@ attr_reader :id, :name, :username, :telephone_number, :email_address
       email_address: result[0]['email_address'])
   end
 
+  def self.find(id)
+    return nil unless id
+
+    result = DatabaseConnection.query("SELECT * FROM users WHERE id = '#{id}'")
+
+    User.new(id: result[0]['id'],
+      name: result[0]['name'],
+      username: result[0]['username'],
+      telephone_number: result[0]['telephone_number'],
+      email_address: result[0]['email_address'])
+  end
+
   def self.already_registered?(email:, username:, telephone_number:)
     result = DatabaseConnection.query("SELECT * FROM users
       WHERE email_address = '#{email}' OR username = '#{username}' OR telephone_number = '#{telephone_number}'")
     result.any?
+  end
+
+  def self.authenticate(email:, password:)
+    result = DatabaseConnection.query("SELECT * FROM users
+      WHERE email = '#{email}'")
+
+    return unless result.any?
+    return unless BCrypt::Password.new(result[0]['password']) == password
+
+    User.new(id: result[0]['id'],
+      name: result[0]['name'],
+      username: result[0]['username'],
+      telephone_number: result[0]['telephone_number'],
+      email_address: result[0]['email_address'])
   end
 end
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -1,30 +1,60 @@
 require 'user'
 require 'database_helper'
 
-
 describe User do
   before(:each) do
-    @user = User.create(
-     name: 'Donald',
-     username: 'DDowg',
-     telephone_number: '0755555555',
-     email_address: 'DDowd@duckmail.com',
-     password: 'IhatedaffyD123'
-     )
+    @user = User.create(name: 'Donald',
+    username: 'DDowg',
+    telephone_number: '0755555555',
+    email_address: 'DDowd@duckmail.com',
+    password: 'IhatedaffyD123')
   end
 
-  describe "create method" do
-    it "creates a new user" do
-
+  describe '#create' do
+    it 'creates a new user' do
 
       persisted_data = persisted_data(table: 'users', id: @user.id)
 
-     expect(@user).to be_a User
-     expect(@user.id).to eq persisted_data['id']
-     expect(@user.name).to eq 'Donald'
-     expect(@user.telephone_number).to eq '0755555555'
-     expect(@user.username).to eq 'DDowg'
-     expect(@user.email_address).to eq 'DDowd@duckmail.com'
+      expect(@user).to be_a User
+      expect(@user.id).to eq persisted_data['id']
+      expect(@user.name).to eq 'Donald'
+      expect(@user.username).to eq 'DDowg'
+      expect(@user.telephone_number).to eq '0755555555'
+      expect(@user.email_address).to eq 'IhatedaffyD123'
+    end
+  end
+
+  describe '#find' do
+    it 'finds a user by ID' do
+
+      result = User.find(@user.id)
+
+      expect(result.id).to eq @user.id
+      expect(result.name).to eq @user.name
+      expect(result.username).to eq @user.username
+      expect(result.telephone_number).to eq @user.telephone_number
+      expect(result.email_address).to eq @user.email
+    end
+
+    it 'returns nil if there is no ID given' do
+      expect(User.find(nil)).to eq nil
+    end
+  end
+
+  describe '#authenticate' do
+    it 'returns nil given an incorrect email address' do
+      expect(User.authenticate(email: 'nottherightemail@me.com', password: 'IhatedaffyD123')).to be_nil
+    end
+
+    it 'returns nil given an incorrect password' do
+      expect(User.authenticate(email: 'DDowd@duckmail.com', password: 'wrongpassword')).to be_nil
+    end
+
+    it 'returns a user given a correct username and password, if one exists' do
+
+      authenticated_user = User.authenticate(email: 'DDowd@duckmail.com', password: 'IhatedaffyD123')
+
+      expect(authenticated_user.id).to eq @user.id
     end
   end
 
