c73a540d9c490692560d699376e4b6a45031aba6
Tolvic <terence.h.mace@gmail.com>
2018-11-07 15:12:31 +0000

    fixing merge conflicts

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -3,15 +3,19 @@ require 'database_helper'
 
 
 describe User do
+  before(:each) do
+    @user = User.create(
+     name: 'Donald',
+     username: 'DDowg',
+     telephone_number: '0755555555',
+     email_address: 'DDowd@duckmail.com',
+     password: 'IhatedaffyD123'
+     )
+  end
+
   describe "create method" do
     it "creates a new user" do
-     @user = User.create(
-      name: 'Donald',
-      username: 'DDowg',
-      telephone_number: '0755555555',
-      email_address: 'DDowd@duckmail.com',
-      password: 'IhatedaffyD123'
-      )
+
 
       persisted_data = persisted_data(table: 'users', id: @user.id)
 
@@ -22,6 +26,19 @@ describe User do
      expect(@user.username).to eq 'DDowg'
      expect(@user.email_address).to eq 'DDowd@duckmail.com'
     end
+  end
 
+  describe '#already_registered?' do
+   it 'returns true if account is already registered' do
+     expect(User.already_registered?(email: 'DDowd@duckmail.com', username: 'cookiemonster', telephone_number: '0755555556')).to eq true
+
+     expect(User.already_registered?(email: 'test@gmail.com', username: 'DDowg', telephone_number: '0755555556')).to eq true
+
+     expect(User.already_registered?(email: 'test@gmail.com', username: 'cookiemonster', telephone_number: '0755555555')).to eq true
+   end
+
+    it 'returns false when account details have not been reqistered' do
+    expect(User.already_registered?(email: 'test@gmail.com', username: 'cookiemonster', telephone_number: '0755555556')).to eq false
+    end
   end
 end
