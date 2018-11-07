a1f113ca6e8d710e22c4faafa2736fc57fe0997c
Tolvic <terence.h.mace@gmail.com>
2018-11-07 22:04:46 +0000

    updated spaces feature tests with signing in steps before test of feature

--- spec/features/spaces_spec.rb
+++ spec/features/spaces_spec.rb
@@ -1,5 +1,16 @@
 feature 'FEATURE space page' do
   scenario 'it has a button which redirects to listing a new space' do
+    @user = User.create(name: 'Donald',
+      username: 'DDowg',
+      telephone_number: '0755555555',
+      email_address: 'DDowd@duckmail.com',
+      password: 'password123')
+
+    visit '/session/new'
+    fill_in(:username, with: 'DDowg')
+    fill_in(:password, with: 'password123')
+    click_button('Sign in')
+
     visit('/spaces')
     click_button 'List new space'
     expect(page).to have_content 'List new space'
