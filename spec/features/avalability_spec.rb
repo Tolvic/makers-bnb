2f4005fa5036c888ff11861f49c5c188004e7770
bmoishe <moishebarnes@gmail.com>
2018-11-07 16:44:27 +0000

    feature test is giving a false positive

---
+++ spec/features/avalability_spec.rb
@@ -0,0 +1,11 @@
+feature 'FEATURE set availability' do
+  scenario 'set availability' do
+    visit('/spaces/new')
+    fill_in 'space_name', with: 'example'
+    fill_in 'description', with: 'example description'
+    fill_in 'price_per_night', with: 100
+    click_button 'submit'
+    expect(page).to have_content('Set an available date')
+  end
+end
+# This doesnt work
