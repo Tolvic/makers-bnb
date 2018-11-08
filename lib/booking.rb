13639f1a29a704dc3c64b4e62cec9a8953f5cc78
tomlightfoot <tomlightfoot96@gmail.com>
2018-11-08 12:20:14 +0000

    Can approve a booking

--- lib/booking.rb
+++ lib/booking.rb
@@ -21,4 +21,38 @@ class Booking
       result[0]["approval_status"]
     )
   end
+
+  def self.find(space_id)
+    return nil unless space_id
+
+    bookings = DatabaseConnection.query("SELECT * FROM bookings WHERE space_id = '#{space_id}'")
+
+    bookings.map do |booking|
+        Booking.new(booking["id"],
+        booking["user_id"],
+        booking["space_id"],
+        booking["availability_id"],
+        booking["approval_status"]
+      )
+    end
+  end
+
+  def self.approve(id)
+    return nil unless id
+
+    result = DatabaseConnection.query(
+      "UPDATE bookings
+      SET approval_status = 'approved'
+      WHERE id = '#{id}'
+      RETURNING id, user_id, space_id, availability_id, approval_status"
+    )
+
+    Booking.new(
+      result[0]["id"],
+      result[0]["user_id"],
+      result[0]["space_id"],
+      result[0]["availability_id"],
+      result[0]["approval_status"]
+    )
+  end
 end
--- spec/booking_spec.rb
+++ spec/booking_spec.rb
@@ -38,4 +38,50 @@ describe User do
       expect(@booking.availability_id).to eq '1'
     end
   end
+
+  describe '#find' do
+    it 'finds a user by ID' do
+      @space2 = Space.create(user_id: @landlord.id,
+        space_name: 'example',
+        description: 'example description',
+        price_per_night: 100
+      )
+
+      @renter2 = User.create(name: 'John',
+      username: 'Terry',
+      telephone_number: '07555555234',
+      email_address: 'jterry@duckmail.com',
+      password: 'IlovejterryT123')
+
+      Booking.create(space_id: @space.id,
+        user_id: @renter2.id,
+        availability_id: 1,
+      )
+      Booking.create(space_id: @space2.id,
+        user_id: @renter.id,
+        availability_id: 1,
+      )
+
+
+      result = Booking.find(@space.id)
+
+      expect(result.last).to be_a Booking
+      expect(result.length).to eq 2
+      expect(result.last.availability_id).to eq "1"
+      expect(result.last.user_id).to eq @renter2.id
+      expect(result.last.space_id).to eq @space.id
+      expect(result.last.approval_status).to eq "pending"
+    end
+
+    it 'returns nil if there is no ID given' do
+      expect(User.find(nil)).to eq nil
+    end
+  end
+
+  describe '#approval' do
+    it 'changes the status of a booking to approved' do
+      @booking = Booking.approve(@booking.id)
+      expect(@booking.approval_status).to eq "approved"
+    end
+  end
 end
